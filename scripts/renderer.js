0115e2150d81c77149a429bdb18332be998551b5
mont8101 <mont8101@stthomas.edu>
2022-02-25 17:34:04 -0600

    +rectangle, circle not working yet

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -46,22 +46,22 @@ class Renderer {
 
     // ctx:          canvas context
     drawSlide0(ctx) {
-        
+        return this.drawRectangle({x: 200, y: 150}, {x: 600, y: 500}, [18, 89, 255, 255], ctx);
     }
 
     // ctx:          canvas context
     drawSlide1(ctx) {
-
+        return this.drawCircle({x: 400, y: 300}, 200, [1, 255, 3, 255], ctx);
     }
 
     // ctx:          canvas context
     drawSlide2(ctx) {
-
+        return this.drawBezierCurve(150, 200, 150, 100, [1, 255, 3, 255], ctx);
     }
 
     // ctx:          canvas context
     drawSlide3(ctx) {
-
+        this.drawLine({x: 200, y: 100}, {x: 400, y: 250}, [255, 255, 3, 255], ctx);
     }
 
     // left_bottom:  object ({x: __, y: __})
@@ -69,7 +69,10 @@ class Renderer {
     // color:        array of int [R, G, B, A]
     // ctx:          canvas context
     drawRectangle(left_bottom, right_top, color, ctx) {
-        
+        this.drawLine(left_bottom, {x: left_bottom.x, y: right_top.y}, color, ctx);
+        this.drawLine({x: left_bottom.x, y: right_top.y}, right_top, color, ctx);
+        this.drawLine(right_top, {x: right_top.x, y: left_bottom.y}, color, ctx);
+        this.drawLine({x: right_top.x, y: left_bottom.y}, left_bottom, color, ctx);
     }
 
     // center:       object ({x: __, y: __})
@@ -77,7 +80,22 @@ class Renderer {
     // color:        array of int [R, G, B, A]
     // ctx:          canvas context
     drawCircle(center, radius, color, ctx) {
-        
+        let tot_pts = this.num_curve_sections;  
+        let deg = 0;
+        let deg_inc = 360/tot_pts;
+        let nextX = center.x + radius * Math.cos(360);
+        let nextY = center.y + radius * Math.sin(360);
+
+        for(let i = 0; i < tot_pts; i++){
+                let x0 = nextX
+                let y0 = nextY
+                deg = deg + deg_inc;
+                let x1 = center.x + radius * Math.cos(deg);
+                let y1 = center.y + radius * Math.sin(deg);
+                this.drawLine({x: x0, y: y0}, {x: x1, y: y1}, [0, 255, 0, 255], ctx);
+                nextX = x1;
+                nextY = y1;
+        }
     }
 
     // pt0:          object ({x: __, y: __})
